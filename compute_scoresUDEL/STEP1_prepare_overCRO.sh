d6518e593909efe60385999264aa7f45c977cac6
regcm <regcm@vihor.gric.dhz.hr>
2018-03-26 14:02:35 +0000

    adding script for the comparions of some scores wrt to UDEL dataset

---
+++ compute_scoresUDEL/STEP1_prepare_overCRO.sh
@@ -0,0 +1,15 @@
+
+
+INPUT1=/home1/regcm/DISK_zemlja/OBS/UDEL/udel_1900_2010_precip_corr_v301.nc
+INPUT2=/home1/regcm/DISK_zemlja/OBS/EOBS_v11/rr_0.25deg_reg_v11.0.nc
+OUTPUT=/work/regcm/temp/test_CARE_001/
+
+LONs=13
+LONe=20
+LATs=42
+LATe=47
+D1=1989-03-01
+D2=2008-11-30
+
+	cdo -f nc -setday,15 -monmean -seldate,$D1,$D2 -sellonlatbox,$LONs,$LONe,$LATs,$LATe ${INPUT1} ${OUTPUT}/2D_UDEL.nc
+	cdo -f nc -setday,15 -monmean -seldate,$D1,$D2 -sellonlatbox,$LONs,$LONe,$LATs,$LATe ${INPUT2} ${OUTPUT}/2D_EOBS.nc
