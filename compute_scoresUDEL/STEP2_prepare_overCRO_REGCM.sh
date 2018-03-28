ba5fe28e3652d3b12c843f5e17ed58da0f12ce83
regcm <regcm@vihor.gric.dhz.hr>
2018-03-28 07:26:49 +0000

    Adding 50km, MIT

--- compute_scoresUDEL/STEP2_prepare_overCRO_REGCM.sh
+++ compute_scoresUDEL/STEP2_prepare_overCRO_REGCM.sh
@@ -1,6 +1,3 @@
-INPUT=/home1/regcm/DISK_CARE/FROM_SRCE/evaluation_grl
-OUTPT=/work/regcm/temp/test_CARE_001
-
 LONs=13
 LONe=20
 LATs=42
@@ -8,11 +5,34 @@ LATe=47
 D1=1989-03-01
 D2=2008-11-30
 
+
+
+#--------------------------------------------------------------------
+#----------------------------------------------------------- 50km+GRL
+#--------------------------------------------------------------------
+INPUT=/home1/regcm/DISK_CARE/FROM_SRCE/evaluation_grl
+OUTPT=/work/regcm/temp/test_CARE_001
+
 # Join files	
 	cdo copy ${INPUT}/EUROPE*SRF*nc ${OUTPT}/tempCOPY.nc
 
 # Computation
 	cdo -f nc -sellonlatbox,$LONs,$LONe,$LATs,$LATe -remapnn,UDEL_grid.txt -settime,12:00:00 -setday,15 -seasmean -seldate,$D1,$D2 -selvar,tpr ${OUTPT}/tempCOPY.nc ${OUTPT}/2D_REGCM50km_GRL_ERAINT.nc
+
+# cleanning
+	rm -vf ${OUTPT}/tempCOPY.nc
+
+#--------------------------------------------------------------------
+#----------------------------------------------------------- 50km+MIT
+#--------------------------------------------------------------------
+INPUT=/home1/regcm/DISK_zemlja/CORDEX/TEST/RegCM-version/output_CORDEX_MAR2012/MONTHLY_MEANS
+OUTPT=/work/regcm/temp/test_CARE_001
+
+# Join files	
+	cdo copy ${INPUT}/EUROPE_SRF.??????.nc ${OUTPT}/tempCOPY.nc
+
+# Computation
+	cdo -f nc -sellonlatbox,$LONs,$LONe,$LATs,$LATe -remapnn,UDEL_grid.txt -settime,12:00:00 -setday,15 -seasmean -seldate,$D1,$D2 -selvar,tpr ${OUTPT}/tempCOPY.nc ${OUTPT}/2D_REGCM50km_MIT_ERAINT.nc
 
 # cleanning
 	rm -vf ${OUTPT}/tempCOPY.nc
--- compute_scoresUDEL/STEP5_analysis.sh
+++ compute_scoresUDEL/STEP5_analysis.sh
@@ -2,7 +2,7 @@
 	INPT=/home1/regcm/DISK_WORK/temp/test_CARE_001
 
 	# Prepare timeseries of area averaged, and area-wide temporal averaged
-	for SRC in UDEL EOBS REGC; do
+	for SRC in UDEL EOBS REGCM50km_GRL; do
 		cdo splitseas -fldmean   ${INPT}/2D_${SRC}_masked.nc ${INPT}/2D_${SRC}_masked_CRO_timeseries_
 		cdo splitseas -yseasmean ${INPT}/2D_${SRC}_masked.nc ${INPT}/2D_${SRC}_masked_CRO_maps_
 	done
@@ -13,11 +13,11 @@
 	# Metric 1: fldcor
 	for SRC in UDEL EOBS; do
 	for SES in DJF  JJA ; do
-	cdo fldcor ${INPT}/2D_REGC_masked_CRO_maps_${SES}.nc ${INPT}/2D_${SRC}_masked_CRO_maps_${SES}.nc ${INPT}/RESULTS/fldcor_${SES}_RegCMvs${SRC}.nc
+	cdo fldcor ${INPT}/2D_REGCM50km_GRL_masked_CRO_maps_${SES}.nc ${INPT}/2D_${SRC}_masked_CRO_maps_${SES}.nc ${INPT}/RESULTS/fldcor_${SES}_REGCM50km_GRLvs${SRC}.nc
 	done
 	done
 	# Metric 2: fldstd
-	for SRC in REGC UDEL EOBS; do
+	for SRC in REGCM50km_GRL UDEL EOBS; do
 	for SES in DJF  JJA ; do
 	cdo fldstd1 ${INPT}/2D_${SRC}_masked_CRO_maps_${SES}.nc ${INPT}/RESULTS/fldstd_${SRC}_masked_CRO_maps_${SES}.nc
 	done
@@ -25,12 +25,24 @@
 	# Metric 3: timcor
 	for SRC in UDEL EOBS; do
 	for SES in DJF  JJA ; do
-	cdo timcor ${INPT}/2D_REGC_masked_CRO_timeseries_${SES}.nc ${INPT}/2D_${SRC}_masked_CRO_timeseries_${SES}.nc ${INPT}/RESULTS/timcor_${SES}_RegCMvs${SRC}.nc
+	cdo timcor ${INPT}/2D_REGCM50km_GRL_masked_CRO_timeseries_${SES}.nc ${INPT}/2D_${SRC}_masked_CRO_timeseries_${SES}.nc ${INPT}/RESULTS/timcor_${SES}_REGCM50km_GRLvs${SRC}.nc
 	done
 	done
 	# Metric 4: timstd
-	for SRC in REGC UDEL EOBS; do
+	for SRC in REGCM50km_GRL UDEL EOBS; do
 	for SES in DJF  JJA ; do
 	cdo timstd1 ${INPT}/2D_${SRC}_masked_CRO_timeseries_${SES}.nc ${INPT}/RESULTS/timstd_${SRC}_masked_CRO_timeseries_${SES}.nc
+	done
+	done
+	# Metric 5: RIAV
+	for SRC in UDEL EOBS; do
+	for SES in DJF  JJA ; do
+	cdo div ${INPT}/RESULTS/timstd_REGCM50km_GRL_masked_CRO_timeseries_${SES}.nc ${INPT}/RESULTS/timstd_${SRC}_masked_CRO_timeseries_${SES}.nc ${INPT}/RESULTS/RIAV_REGCM50km_GRLvs${SRC}_masked_CRO_timeseries_${SES}.nc
+	done
+	done
+	# Metric 5: RSV
+	for SRC in UDEL EOBS; do
+	for SES in DJF  JJA ; do
+	cdo div ${INPT}/RESULTS/fldstd_REGCM50km_GRL_masked_CRO_maps_${SES}.nc ${INPT}/RESULTS/fldstd_${SRC}_masked_CRO_maps_${SES}.nc ${INPT}/RESULTS/RSV_REGCM50km_GRLvs${SRC}_masked_CRO_maps_${SES}.nc
 	done
 	done
