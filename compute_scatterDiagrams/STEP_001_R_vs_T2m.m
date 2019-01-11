684a80a74a1e21487032eae460d6d81e0e61df36
Ivo Sanader <ivan.guettler@gmail.com>
2019-01-11 10:30:45 +0100

    Change marker size

--- compute_scatterDiagrams/STEP_001_R_vs_T2m.m
+++ compute_scatterDiagrams/STEP_001_R_vs_T2m.m
@@ -43,7 +43,8 @@ style{1}="r o";
 style{2}="b v";
 style{3}="g s";
 style{4}="k d";
-FUTA=7
+FUTA=7;
+tip=1;
 %-----------
 % Determine share of data -------------------------------------------------------------------------------------------> 12.5km
 %-----------
@@ -102,17 +103,17 @@ end
 %-----------
 % Plot data
 %-----------
-loc_Q_x=[-7  4.5 -7  4.5];
-loc_Q_y=[14   14 -5   -5];
+loc_Q_x=[-6.5  3.5 -6.5  3.5];
+loc_Q_y=[ 12   12  -1.5 -1.5];
 f=figure(1); set(f,'outerposition',[276   225   730   799],'position',[286   257   710   664]);
 
 	subplot(2,2,1)
 		plot([ 0 0],[-5 13],'k'); hold on
 		plot([-7 5],[ 0  0],'k'); hold on
-		plot(reshape(squeeze(t_GCM_11_GRL(:,:,1)),Nx*Ny,1),reshape(squeeze(r_GCM_11_GRL(:,:,1)),Nx*Ny,1),style{1}); hold on
-		plot(reshape(squeeze(t_GCM_11_MIT(:,:,1)),Nx*Ny,1),reshape(squeeze(r_GCM_11_MIT(:,:,1)),Nx*Ny,1),style{2}); hold on
-		plot(reshape(squeeze(t_ERA_11_GRL(:,:,1)),Nx*Ny,1),reshape(squeeze(r_ERA_11_GRL(:,:,1)),Nx*Ny,1),style{3}); hold on
-		plot(reshape(squeeze(t_ERA_11_MIT(:,:,1)),Nx*Ny,1),reshape(squeeze(r_ERA_11_MIT(:,:,1)),Nx*Ny,1),style{4}); 
+	       	plot(reshape(squeeze(t_GCM_11_GRL(:,:,1)),Nx*Ny,1),reshape(squeeze(r_GCM_11_GRL(:,:,1)),Nx*Ny,1),style{1},'markersize',tip); hold on
+ 		plot(reshape(squeeze(t_GCM_11_MIT(:,:,1)),Nx*Ny,1),reshape(squeeze(r_GCM_11_MIT(:,:,1)),Nx*Ny,1),style{2},'markersize',tip); hold on
+		plot(reshape(squeeze(t_ERA_11_GRL(:,:,1)),Nx*Ny,1),reshape(squeeze(r_ERA_11_GRL(:,:,1)),Nx*Ny,1),style{3},'markersize',tip); hold on
+		plot(reshape(squeeze(t_ERA_11_MIT(:,:,1)),Nx*Ny,1),reshape(squeeze(r_ERA_11_MIT(:,:,1)),Nx*Ny,1),style{4},'markersize',tip); 
 			xlabel('dT2m (deg C)'); ylabel('dR (mm/d)')
 			xlim([-7 5]); ylim([-5 13])
 			title('12.5km DJF')
@@ -125,10 +126,10 @@ f=figure(1); set(f,'outerposition',[276   225   730   799],'position',[286   257
 	subplot(2,2,3)
 		plot([ 0 0],[-5 13],'k'); hold on
 		plot([-7 5],[ 0  0],'k'); hold on
-		plot(reshape(squeeze(t_GCM_11_GRL(:,:,3)),Nx*Ny,1),reshape(squeeze(r_GCM_11_GRL(:,:,3)),Nx*Ny,1),style{1}); hold on
-		plot(reshape(squeeze(t_GCM_11_MIT(:,:,3)),Nx*Ny,1),reshape(squeeze(r_GCM_11_MIT(:,:,3)),Nx*Ny,1),style{2}); hold on
-		plot(reshape(squeeze(t_ERA_11_GRL(:,:,3)),Nx*Ny,1),reshape(squeeze(r_ERA_11_GRL(:,:,3)),Nx*Ny,1),style{3}); hold on
-		plot(reshape(squeeze(t_ERA_11_MIT(:,:,3)),Nx*Ny,1),reshape(squeeze(r_ERA_11_MIT(:,:,3)),Nx*Ny,1),style{4}); 
+		plot(reshape(squeeze(t_GCM_11_GRL(:,:,3)),Nx*Ny,1),reshape(squeeze(r_GCM_11_GRL(:,:,3)),Nx*Ny,1),style{1},'markersize',tip); hold on
+		plot(reshape(squeeze(t_GCM_11_MIT(:,:,3)),Nx*Ny,1),reshape(squeeze(r_GCM_11_MIT(:,:,3)),Nx*Ny,1),style{2},'markersize',tip); hold on
+		plot(reshape(squeeze(t_ERA_11_GRL(:,:,3)),Nx*Ny,1),reshape(squeeze(r_ERA_11_GRL(:,:,3)),Nx*Ny,1),style{3},'markersize',tip); hold on
+		plot(reshape(squeeze(t_ERA_11_MIT(:,:,3)),Nx*Ny,1),reshape(squeeze(r_ERA_11_MIT(:,:,3)),Nx*Ny,1),style{4},'markersize',tip); 
 			xlabel('dT2m (deg C)'); ylabel('dR (mm/d)')
 			xlim([-7 5]); ylim([-5 13])
 			title('12.5km JJA')
@@ -199,10 +200,10 @@ end
 	subplot(2,2,2)
 		plot([ 0 0],[-5 13],'k'); hold on
 		plot([-7 5],[ 0  0],'k'); hold on
-		plot(reshape(squeeze(t_GCM_44_GRL(:,:,1)),cNx*cNy,1),reshape(squeeze(r_GCM_44_GRL(:,:,1)),cNx*cNy,1),style{1}); hold on
-		plot(reshape(squeeze(t_GCM_44_MIT(:,:,1)),cNx*cNy,1),reshape(squeeze(r_GCM_44_MIT(:,:,1)),cNx*cNy,1),style{2}); hold on
-		plot(reshape(squeeze(t_ERA_44_GRL(:,:,1)),cNx*cNy,1),reshape(squeeze(r_ERA_44_GRL(:,:,1)),cNx*cNy,1),style{3}); hold on
-		plot(reshape(squeeze(t_ERA_44_MIT(:,:,1)),cNx*cNy,1),reshape(squeeze(r_ERA_44_MIT(:,:,1)),cNx*cNy,1),style{4}); 
+		plot(reshape(squeeze(t_GCM_44_GRL(:,:,1)),cNx*cNy,1),reshape(squeeze(r_GCM_44_GRL(:,:,1)),cNx*cNy,1),style{1},'markersize',tip); hold on
+		plot(reshape(squeeze(t_GCM_44_MIT(:,:,1)),cNx*cNy,1),reshape(squeeze(r_GCM_44_MIT(:,:,1)),cNx*cNy,1),style{2},'markersize',tip); hold on
+		plot(reshape(squeeze(t_ERA_44_GRL(:,:,1)),cNx*cNy,1),reshape(squeeze(r_ERA_44_GRL(:,:,1)),cNx*cNy,1),style{3},'markersize',tip); hold on
+		plot(reshape(squeeze(t_ERA_44_MIT(:,:,1)),cNx*cNy,1),reshape(squeeze(r_ERA_44_MIT(:,:,1)),cNx*cNy,1),style{4},'markersize',tip); 
 			xlabel('dT2m (deg C)'); ylabel('dR (mm/d)')
 			xlim([-7 5]); ylim([-5 13])
 			title('50km DJF')
@@ -213,10 +214,10 @@ end
 				t=text(loc_Q_x(Q),loc_Q_y(Q)-3,[num2str(round(d_Q(Q,1)./N_Q*100*10)/10),'%']); set(t,'Fontsize',FUTA,'Color','k');
 			end
 	subplot(2,2,4)
-		plot(reshape(squeeze(t_GCM_44_GRL(:,:,3)),cNx*cNy,1),reshape(squeeze(r_GCM_44_GRL(:,:,3)),cNx*cNy,1),style{1}); hold on
-		plot(reshape(squeeze(t_GCM_44_MIT(:,:,3)),cNx*cNy,1),reshape(squeeze(r_GCM_44_MIT(:,:,3)),cNx*cNy,1),style{2}); hold on
-		plot(reshape(squeeze(t_ERA_44_GRL(:,:,3)),cNx*cNy,1),reshape(squeeze(r_ERA_44_GRL(:,:,3)),cNx*cNy,1),style{3}); hold on
-		plot(reshape(squeeze(t_ERA_44_MIT(:,:,3)),cNx*cNy,1),reshape(squeeze(r_ERA_44_MIT(:,:,3)),cNx*cNy,1),style{4}); 
+		plot(reshape(squeeze(t_GCM_44_GRL(:,:,3)),cNx*cNy,1),reshape(squeeze(r_GCM_44_GRL(:,:,3)),cNx*cNy,1),style{1},'markersize',tip); hold on
+		plot(reshape(squeeze(t_GCM_44_MIT(:,:,3)),cNx*cNy,1),reshape(squeeze(r_GCM_44_MIT(:,:,3)),cNx*cNy,1),style{2},'markersize',tip); hold on
+		plot(reshape(squeeze(t_ERA_44_GRL(:,:,3)),cNx*cNy,1),reshape(squeeze(r_ERA_44_GRL(:,:,3)),cNx*cNy,1),style{3},'markersize',tip); hold on
+		plot(reshape(squeeze(t_ERA_44_MIT(:,:,3)),cNx*cNy,1),reshape(squeeze(r_ERA_44_MIT(:,:,3)),cNx*cNy,1),style{4},'markersize',tip); 
 		plot([ 0 0],[-5 13],'k'); hold on
 		plot([-7 5],[ 0  0],'k'); hold on
 			xlabel('dT2m (deg C)'); ylabel('dR (mm/d)')
